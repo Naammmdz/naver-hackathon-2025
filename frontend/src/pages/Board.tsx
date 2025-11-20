6dd804ea9908045903ec1a22487e9db3591759e3
xt2201 <xuanthanh2201.work@gmail.com>
2025-11-20 16:53:06 +0700

    frontend/add-board-component

---
+++ frontend/src/pages/Board.tsx
@@ -0,0 +1,9 @@
+import { BoardView } from "@/components/board/BoardView";
+
+export default function Board() {
+  return (
+    <div className="flex h-full w-full">
+      <BoardView />
+    </div>
+  );
+}
